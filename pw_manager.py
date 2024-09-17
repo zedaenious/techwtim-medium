f361adc15118ecf144215514989949a036e02548
zedaenious <matt@zelenak.me>
2024-09-17 15:55:24 -0400

    add & view methods; move action into while loop

--- pw_manager.py
+++ pw_manager.py
@@ -4,22 +4,30 @@ def add():
   name = input('Username: ')
   pwd = input('Password: ')
 
-  with open('passwords.txt', 'a') as f:
+  #a = append mode
+  with open('pw.txt', 'a') as f:
     f.write(name + ' | ' + pwd + '\n')
 
 def view():
-  pass
-
-action = input('Would you like to (add) a new password, (view) existing passwords, or (quit) the program? ').lower()
+  # r = read mode
+  with open('pw.txt', 'r') as f:
+    for line in f.readlines():
+      data = line.rstrip()
+      name, pwd = data.split(" | ")
+      print('Username:', name, '\nPassword:', pwd + '\n')
 
 while True:
+  action = input('Would you like to (add) a new password, (view) existing passwords, or (quit) the program? ').lower()
+
   if action == 'quit':
     break
 
   if action == 'add':
     add()
+    continue
   elif action == 'view':
     view()
+    continue
   else:
     print('invalid choice')
     continue
