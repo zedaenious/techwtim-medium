e331b63e911a5dcf939d2de32d62bfbdc02df2c3
zedaenious <matt@zelenak.me>
2024-09-19 15:10:40 -0400

    PIG: player array init; increment current score via roll method

--- pig.py
+++ pig.py
@@ -1,5 +1,7 @@
 import random
 
+max_score = 50
+
 def roll():
   min_val = 1
   max_val = 6
@@ -17,3 +19,25 @@ while True:
       print("Selection must be betweeen 2 and 4 players. Please make a valid choice.")
   else:
     print()
+
+player_scores = [0 for p in range(players)]
+
+print(player_scores)
+
+while max(player_scores) < max_score:
+  current_score = 0
+  should_roll = input("Would you like to roll (y)?: ").lower()
+  
+  if should_roll != "y":
+    break
+
+  value = roll()
+
+  if value == 1:
+    print("You rolled a 1, now your turn is done!!")
+    break
+  else:
+    current_score += value
+    print("You rolled a:", value)
+  
+  print("Your wcore is:", current_score)
